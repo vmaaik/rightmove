4b546d823384400d2f46f02c24fc94b844aa5467
Mic <vmaaik@gmail.com>
2023-07-07 15:30:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from geojson_scraper import RightmoveData
 import json
+import logging
 
 app = Flask(__name__)
 
+# Configure logging
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
 # Example URLs
 urls = [
-    "https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=4&propertyTypes=detached%2Csemi-detached%2Cterraced%2Cbungalow&keywords=&sortType=2&viewType=LIST&channel=BUY&index=0&maxPrice=350000&radius=0.0&maxDaysSinceAdded=1&locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22q%7BrdIzkhNgpH%7CoEwrDojGmkFq%7EVb%60BeqPlaAapJp%7DCslCnxCq%7CH%60Ve%7EEqxE%7BiKleJsrSjcN%7DqBlSr_e%40%7CsCruE%7CqHylDbq%40xiK%7EEpwPgsBztXkgFxnZijQixH%22%7D",
-    "https://www.rightmove.co.uk/property-for-sale/find.html?minBedrooms=4&propertyTypes=detached%2Csemi-detached%2Cterraced%2Cbungalow&keywords=&sortType=2&viewType=LIST&channel=BUY&index=0&maxPrice=350000&radius=0.0&maxDaysSinceAdded=1&locationIdentifier=USERDEFINEDAREA%5E%7B%22polylines%22%3A%22kmreIpgrKspAqaAuzBvXajBhRcsAdPmgBa%5D%7BhAajAcn%40qnBhNc%60Ejo%40adClgBuaBhkBc%7B%40%60xBse%40bjBnT%7EhAvv%40rbAllBr%5DjhC%7CUv_CeIjsE%7BPbyAyp%40uI%22%7D"
+    # Your URLs here
 ]
 
 # Cache variable to store the GeoJSON data
@@ -29,7 +32,7 @@ def scrape_data():
     # Convert to JSON string
     geojson_str = json.dumps(geojson_obj)
 
-    print(geojson_str)
+    logging.debug(f"Scraped GeoJSON: {geojson_str}")
 
     return geojson_str
 
@@ -41,6 +44,7 @@ def populate_cache():
 
 @app.route('/')
 def index():
+    logging.debug("Rendering index page")
     return render_template('index.html', geojson=cache)
 
 
